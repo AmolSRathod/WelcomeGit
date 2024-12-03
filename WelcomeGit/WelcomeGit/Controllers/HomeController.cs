9fbf0a66ced3e452f31136b49008695172f4063c
AmolSRathod <amol.csn@gmail.com>
2024-12-03 21:33:45 +0530

    Add Pushkar view

--- WelcomeGit/WelcomeGit/Controllers/HomeController.cs
+++ WelcomeGit/WelcomeGit/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WelcomeGit.Controllers
             return View();
         }
 
+        public IActionResult Pushkar()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
