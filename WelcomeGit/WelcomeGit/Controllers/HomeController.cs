b18e7757b9654f2e39f7a95d3b8d41357b62c586
AmolSRathod <amol.csn@gmail.com>
2024-12-05 12:44:22 +0530

    Added Sachin view

--- WelcomeGit/WelcomeGit/Controllers/HomeController.cs
+++ WelcomeGit/WelcomeGit/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace WelcomeGit.Controllers
             return View();
         }
 
+        public IActionResult Sachin()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
