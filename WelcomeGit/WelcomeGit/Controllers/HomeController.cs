513f19ae5f4619ca9d1ca7a95ba40a141b1909f2
AmolSRathod <amol.csn@gmail.com>
2024-12-05 10:53:24 +0530

    Added Rathod view

--- WelcomeGit/WelcomeGit/Controllers/HomeController.cs
+++ WelcomeGit/WelcomeGit/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace WelcomeGit.Controllers
             return View();
         }
 
+        public IActionResult Rathod()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
