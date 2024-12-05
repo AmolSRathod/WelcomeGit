da95498f1cf4221e9e424b32fc37b07da4d4427e
AmolSRathod <amol.csn@gmail.com>
2024-12-05 10:57:16 +0530

    Added Sanjay view

--- WelcomeGit/WelcomeGit/Controllers/HomeController.cs
+++ WelcomeGit/WelcomeGit/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace WelcomeGit.Controllers
             return View();
         }
 
+        public IActionResult Sanjay()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
